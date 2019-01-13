7a5eb70699f4e6b157432c39fdb4c7ef9beaa019
CoolItTheGreat <46637491+CoolItTheGreat@users.noreply.github.com>
2019-01-13 13:14:37 +1000

    Added StockManagement Prodject Code

---
+++ StockManagement/StockManagementMain.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StockManagement
+{
+    public partial class StockManagementMain : Form
+    {
+        public StockManagementMain()
+        {
+            InitializeComponent();
+        }
+
+        private void StockManagementMain_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Products pro = new Products();
+            pro.MdiParent = this;
+            pro.Show();
+        }
+    }
+}
